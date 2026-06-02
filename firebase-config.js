a358af09d894e6c78db097ff72c9da408a52db64
Nicolas Ramos <naramosa@fondoune.com>
2026-06-02 15:22:36 -0500

    Publicar portales FondoUne

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-kpFWfUOA2CAszogW4ODgCz1pQfYpBms",
+    authDomain: "fondoune-credito.firebaseapp.com",
+    projectId: "fondoune-credito",
+    storageBucket: "fondoune-credito.firebasestorage.app",
+    messagingSenderId: "94345899414",
+    appId: "1:94345899414:web:dcba4bd86fd4132b8901b7",
+    measurementId: "G-C7ZKXX7P4P"
+  };
+  
+  window.FondouneFirebaseConfig = firebaseConfig;
